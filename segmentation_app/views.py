854e55cd8bc1c0e432267da38d19a329b622b33b
Patrycja Gorzelańczyk <48128614+pattrycja96@users.noreply.github.com>
2019-08-10 10:41:33 +0200

    adding 2-connected-components

--- segmentation_app/views.py
+++ segmentation_app/views.py
@@ -22,6 +22,7 @@ def home(request):
 def segmentation(request):
     if request.method == 'POST':
         context.clear()
+        #context={}
         uploaded_file = request.FILES['image']
         fs = FileSystemStorage()
         name = fs.save(uploaded_file.name, uploaded_file)
@@ -33,10 +34,10 @@ def tracking(request):
     return render(request, 'home.html')
 
 
+
 def mst(request):
     print(context)
     if request.method == 'POST':
-
         edges8=request.POST.get("Edges")
         const=float(request.POST.get("Const"))
         min_size=int(request.POST.get("Min_size"))
@@ -58,42 +59,32 @@ def mst(request):
         result[0].save('static/media/temporary.png')
         context['segmented_image'] = "/static/media/temporary.png"
         context['counter'] = result[1]
-        context['forest'] = result[2]
-        context['graph'] = result[3]
     return render(request, 'mst.html', context)
 
 
-def mst_additional(request):
+def two_cc(request):
     print(context)
     if request.method == 'POST':
-
+        channel=int(request.POST.get("Channel"))
         const=float(request.POST.get("Const"))
-        print(const)
-
-        min_size=int(request.POST.get("Min_size"))
         threshold=int(request.POST.get("Threshold"))
-        max_size=int(request.POST.get("Max_size"))
-        forest=context['forest']
-        G=context['graph']
+        fill_in=int(request.POST.get("Fill_in"))
         if threshold==1:
-            result= seg.mst_1const_additional(G, forest,
-                                            threshold=threshold_mst_1,
-                                            const=const,min_size=min_size,max_size=max_size)
-        elif threshold==2:
-            result= seg.mst_1const_additional(G, forest,
-                                            threshold=threshold_mst_2,
-                                            const=const,min_size=min_size,max_size=max_size)
-        elif threshold==3:
-            result= seg.mst_1const_additional(G, forest,
-                                            threshold=threshold_mst_3,
-                                            const=const,min_size=min_size,max_size=max_size)
+            const=None
+        if channel==1:
+            result = seg.two_connected_components(Image.open(context['image']), fill_in=fill_in, thresh=const)
+        elif channel==2:
+            result= seg.two_connected_components(Image.open(context['image']),channel="red",  fill_in=fill_in, thresh=const)
+        elif channel==3:
+            result= seg.two_connected_components(Image.open(context['image']),channel="green",  fill_in=fill_in, thresh=const)
+        elif channel==4:
+            result= seg.two_connected_components(Image.open(context['image']),channel="blue",  fill_in=fill_in, thresh=const)
         else:
             print("Problem");
         result[0].save('static/media/temporary.png')
         context['segmented_image'] = "/static/media/temporary.png"
         context['counter'] = result[1]
-    return render(request, 'mst.html', context)
-
+    return render(request, 'two_cc.html', context)
 
 
 @ensure_csrf_cookie
@@ -102,6 +93,8 @@ def choose_alg(request):
         name = request.POST.get("algos")
         if(name == "mst"):
             return render(request, 'mst.html', context)
+        if(name == "two_cc"):
+            return render(request, 'two_cc.html', context)
         else:
             return HttpResponseForbidden('Something is wrong, check if you filled all required positions!2')
 
